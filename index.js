d616317f7e32492fe4931abf8ebfdc2c2623f2c7
Colleen Cabedo <cmcabedo@gmail.com>
2022-09-19 16:13:10 -0400

    commit tree e23930da6a7e349d13d8e56ce34ee549beab986f

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun (activity) {
+    switch (activity) {
+        case 'bathe my dog' :
+            return `This Saturday, I want to bathe my dog!`
+            break
+        default:
+            return `This Saturday, I want to roller-skate!`
+            break
+    }
+}
+saturdayFun();
+
+function mondayWork (activity) {
+    switch (activity) {
+        case 'go to the office' :
+            return `This Monday, I will go to the office.`
+            break
+        default:
+            return `This Monday, I will work from home.`
+            break
+    }
+}
+mondayWork();
